c1d99bb1897990015c384cc930efe40313648fa1
Talibert <guilhermetaliberti@gmail.com>
2026-03-24 22:59:15 -0300

    Injetando UserMapper

--- src/main/java/com/example/api_docker/infra/persistence/mapper/UserMapper.java
+++ src/main/java/com/example/api_docker/infra/persistence/mapper/UserMapper.java
@@ -2,14 +2,16 @@ package com.example.api_docker.infra.persistence.mapper;
 
 import com.example.api_docker.infra.persistence.entity.UserJpaEntity;
 import com.example.api_docker.user.entity.User;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserMapper {
 
-    public static User toDomain(UserJpaEntity entity) {
+    public User toDomain(UserJpaEntity entity) {
         return new User(entity.getId(), entity.getName());
     }
 
-    public static UserJpaEntity toJpa(User user) {
+    public UserJpaEntity toJpa(User user) {
         UserJpaEntity entity = new UserJpaEntity(user.getName());
         entity.setId(user.getId());
 
